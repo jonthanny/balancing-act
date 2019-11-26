b757dcf20f2e4b059602013528a4783add6690c5
Lane Hunter <lanehunter.96@gmail.com>
2019-11-25 17:35:36 -0700

    Hide dashboard page on transactions icon click

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var banner = document.querySelector(".banner");
 var dashIcon = document.querySelector(".dashIcon")
 var transIcon = document.querySelector(".transIcon");
 var profileIcon = document.querySelector(".profileIcon");
+var dashPage = document.querySelector(".dashPage");
 
 closeIcon.addEventListener('click' , closeBanner);
 window.addEventListener('load' , highlightDashIcon);
@@ -15,24 +16,26 @@ dashIcon.addEventListener('click' , highlightDashIcon);
 transIcon.addEventListener('click' , highlightTransIcon);
 profileIcon.addEventListener('click' , highlightProfileIcon);
 
-dashIcon.addEventListener('mouseover' , highlightDashIcon);
-transIcon.addEventListener('mouseover' , highlightTransIcon);
-profileIcon.addEventListener('mouseover' , highlightProfileIcon);
-
-dashIcon.addEventListener('mouseout' , highlightDashIcon);
-transIcon.addEventListener('mouseout' , highlightTransIcon);
-profileIcon.addEventListener('mouseout' , highlightProfileIcon);
+// dashIcon.addEventListener('mouseover' , hoverDashIcon);
+// transIcon.addEventListener('mouseover' , hoverTransIcon);
+// profileIcon.addEventListener('mouseover' , hoverProfileIcon);
+//
+// dashIcon.addEventListener('mouseout' , highlightDashIcon);
+// transIcon.addEventListener('mouseout' , highlightTransIcon);
+// profileIcon.addEventListener('mouseout' , highlightProfileIcon);
 
 function highlightDashIcon() {
   dashIcon.classList.add("dashIconClicked");
   transIcon.classList.remove("transIconClicked");
   profileIcon.classList.remove("profileIconClicked");
+  dashPage.style.display = "block";
 }
 
 function highlightTransIcon() {
   transIcon.classList.add("transIconClicked");
   dashIcon.classList.remove("dashIconClicked");
   profileIcon.classList.remove("profileIconClicked");
+  dashPage.style.display = "none";
 }
 
 function highlightProfileIcon() {
